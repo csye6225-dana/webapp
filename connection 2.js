979649366ac11d72a747444b41d2fdc13943c265
Dana-Guo <80471259+Dana-Guo@users.noreply.github.com>
2024-02-06 23:31:12 -0500

    first update

---
+++ connection 2.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('cyse6225',process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: 'localhost',
+  port: 3306,
+  dialect: 'mysql'
+});
+
+module.exports = sequelize;
---
+++ main 2.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sequelize = require('./connection');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+
+// Middleware to handle payload 
+app.use('/healthz', (req, res, next) => {
+    if (req.method !== 'GET') {
+    console.error('Method Not Allowed: ', req.method);
+    return res.status(405).header('Cache-Control', 'no-cache').send(); // Method Not Allowed
+    }
+    if (req.headers['content-length']) {
+      console.error('Bad Request');
+      return res.status(400).header('Cache-Control', 'no-cache').send(); // Bad Request
+    }
+  next();
+});
+
+// Health Check Endpoint
+app.get('/healthz', async (req, res) => {
+  try {
+    // Check database connectivity
+    await sequelize.authenticate();
+    console.log('Connection succeed!');
+    res.status(200).header('Cache-Control', 'no-cache').send();
+  } catch (error) {
+    console.error('Database connection error:\n', error);
+    res.status(503).header('Cache-Control', 'no-cache').send();
+  }
+});
+
+// Run the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
