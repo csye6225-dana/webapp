a68f8d38a985da6ee151b7bb1b0fc3d464cc6e91
Dana-Guo <80471259+Dana-Guo@users.noreply.github.com>
2024-02-06 23:10:32 -0500

    add console.log for each error

--- main.js
+++ main.js
@@ -4,19 +4,20 @@ const bodyParser = require('body-parser');
 const sequelize = require('./connection');
 
 const app = express();
-const PORT = process.env.PORT || 8888;
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 
 // Middleware to handle payload 
 app.use('/healthz', (req, res, next) => {
-    if (req.headers['content-length'] > 0) {
-        return res.status(400).header('Cache-Control', 'no-cache').send(); // Bad Request
-      }
     if (req.method !== 'GET') {
+    console.log('Method Not Allowed: ', req.method);
     return res.status(405).header('Cache-Control', 'no-cache').send(); // Method Not Allowed
     }
-  
+    if (req.headers['content-length']) {
+      console.log('Bad Request');
+      return res.status(400).header('Cache-Control', 'no-cache').send(); // Bad Request
+    }
   next();
 });
 
@@ -27,7 +28,7 @@ app.get('/healthz', async (req, res) => {
     await sequelize.authenticate();
     res.status(200).header('Cache-Control', 'no-cache').send();
   } catch (error) {
-    console.error('Database connection error:', error);
+    console.error('Database connection error:\n', error);
     res.status(503).header('Cache-Control', 'no-cache').send();
   }
 });
