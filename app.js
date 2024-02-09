8e482af9eebc9d4aa24fc9f7f9e433a5f6c06bae
Dana <80471259+Dana-Guo@users.noreply.github.com>
2024-02-08 22:49:23 -0500

    02 (#2)
    * rewrite assignment 1
    
    * fix 400 error
    
    * add console.log for each error
    
    * rewrite assignment 1
    
    * delete
    
    * first update
    
    * update part of code
    
    * update
    
    * part of
    
    * bootsrap
    
    * update
    
    * update
    
    * install package
    
    * change id definition
    
    * Update userController.js
    
    * update
    
    * readjust method
    
    * update
    
    * update
    
    * debug
    
    * finish
    
    * Delete .env
    
    * Add .gitignore file
    
    * Update .gitignore
    
    * reset
    
    * resubmit
    
    * fix error
    
    * Update userController.js
    
    * Add health check

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bootstrap = require('./models/User');
+const healthRouter = require('./routes/healthRouters.js');
+const userRouter = require('./routes/userRouters.js');
+const checkHealthMiddleware = require('./middlewares/checkHealthMiddleware'); // Import checkHealthMiddleware
+const authenticateUser = require('./middlewares/authMiddleware.js');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+
+// Initializing 
+const initializeApp = async () => {
+  try {
+    // Bootstrap the database
+    await bootstrap.sync({ alter: true }); 
+    console.log('Bootstrap the Database successfully!');
+  } catch (error) {
+    console.error('Error initializing app:', error.message);
+  }
+};
+
+// Attach health router with health check middleware
+app.use('/healthz', checkHealthMiddleware, healthRouter);
+
+// Attach user router
+app.use('/v1/user', userRouter);
+// Authentication middleware for GET and PUT request
+app.use('/v1/user', (req, res, next) => {
+  if (req.method === 'GET' || req.method === 'PUT') {
+    authenticateUser(req, res, next);
+  } else {
+    next();
+  }
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error('Error:', err);
+  res.status(err.status).json({ error: err.message });
+});
+
+// Run the server
+app.listen(PORT, async () => {
+  console.log(`Server is running on port ${PORT}`);
+  await initializeApp();
+});
---
+++ controllers/userController.js
@@ -0,0 +1,93 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+//when use Post, create a new user
+const createUser = async (req, res) => {
+  const {username, password, firstName, lastName } = req.body;
+  try {
+    // Create user using Sequelize model
+    const existingUser = await User.findOne({ where: {username} });
+    if (existingUser) {
+      console.log(username, 'already exists. Please use another one.');
+      return res.status(400).json({ error: 'The email address already exists. Please use another one.' });
+    }
+    const newUser = await User.createUser({username, password, firstName, lastName});
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.error('Error creating new user', error);
+    res.status(error.status).json({ error: error.message });
+  }
+};
+
+
+const updateUser = async (req, res) => {
+  const allowedFields = ['username', 'firstName', 'lastName', 'password']; // Allowed fields for update
+  const receivedFields = Object.keys(req.body);
+  const invalidFields = receivedFields.filter(field => !allowedFields.includes(field));
+
+  if (invalidFields.length > 0) {
+    return res.status(400).json({ error: `Invalid field(s): ${invalidFields.join(', ')}` });
+  }
+  try {
+    if (req.user.username !== req.body.username) {
+      return res.status(403).json({ error: "You are not allowed to update another user's account information" });
+    }
+    
+    const user = await User.findOne({ where: { username: req.user.username } });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    // Update 
+    if (req.body.password) {
+      const hashedPassword = await bcrypt.hash(req.body.password, 15);
+      req.body.password = hashedPassword;
+    }
+    const [affectedRows] = await User.update(req.body, {
+      where: { username: req.user.username }
+    });
+
+    // Check if affected 
+    if (affectedRows === 0) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    // Update the account_updated field
+    await User.update({ account_updated: new Date() }, { where: { username: req.user.username } });
+    return res.status(200).json({ result: "Successfully updated"});
+  } catch (error) {
+    console.error('Error updating user:', error);
+    res.status(error.status).json({ error: error.message });
+  }
+};
+
+
+const getUser = async (req, res) => {
+  try {
+    if (req.body.username !== req.user.username) {
+      return res.status(403).json({ error: "You are not allowed to access another user's account information" });
+    }
+
+    // Fetch user data from the database
+    const user = await User.findOne({ where: { username: req.body.username } });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    // Remove sensitive information
+    const otherInfo = Object.assign({}, user.get());
+    delete otherInfo.password;
+    return res.status(200).json(otherInfo);
+  } catch (error) {
+    console.error('Error fetching user:', error);
+    res.status(error.status).json({ error: error.message });
+  }
+};
+
+
+module.exports = {
+    createUser,
+    updateUser,
+    getUser
+}
+
