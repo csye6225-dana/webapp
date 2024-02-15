491f6b6be790ce719ddff77f3bd1f9c2caa2f51f
Dana <80471259+DanaKwoh@users.noreply.github.com>
2024-02-15 13:12:14 -0500

    03 (#12)
    * Update README.md
    
    * update new status check
    
    * Update connection-check.yml
    
    * Update connection-check.yml
    
    * update
    
    * update workflow

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('cyse6225',process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER, process.env.DB_PASSWORD, {
   host: 'localhost',
   port: 3306,
   dialect: 'mysql'
