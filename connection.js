2c5757533cabb1fc5979c3d5c0927411ce5b5fdf
Dana <80471259+DanaKwoh@users.noreply.github.com>
2024-02-15 16:58:21 -0500

    03 (#36)
    * Update README.md
    
    * update new status check
    
    * Update connection-check.yml
    
    * Update connection-check.yml
    
    * update
    
    * update workflow
    
    * update
    
    * test1
    
    * update
    
    * update
    
    * Delete test-draft1.yml
    
    * Update connection-check.yml
    
    * Update connection-check.yml
    
    * Update connection-check.yml
    
    * Update connection-check.yml
    
    * Update connection-check.yml
    
    * Update connection-check.yml
    
    * Update connection-check.yml
    
    * Update connection-check.yml
    
    * update test1
    
    * update
    
    * Update test1.js
    
    * update test 1
    
    * Update test1.js
    
    * Update Test1.yml
    
    * final versoin
    
    * Delete package-lock.json
    
    * Delete package.json
    
    * Delete .DS_Store
    
    * Update connection.js
    
    ---------
    
    Co-authored-by: DanaKwoh <danag@myhost.local>

--- connection.js
+++ connection.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: 'localhost',
-  port: 3306,
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
   dialect: 'mysql'
 });
 
