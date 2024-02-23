ff067d20436454af9a2b8f2922bcaebdfae2322a
Dana <80471259+DanaKwoh@users.noreply.github.com>
2024-02-23 14:38:02 -0500

    04 (#38)
    * Update README.md
    
    * update new status check
    
    * Update connection-check.yml
    
    * Update connection-check.yml
    
    * update
    
    * update workflow
    
    * update
    
    * test1
    
    * update
    
    * update
    
    * Delete test-draft1.yml
    
    * Update connection-check.yml
    
    * Update connection-check.yml
    
    * Update connection-check.yml
    
    * Update connection-check.yml
    
    * Update connection-check.yml
    
    * Update connection-check.yml
    
    * Update connection-check.yml
    
    * Update connection-check.yml
    
    * update test1
    
    * update
    
    * Update test1.js
    
    * update test 1
    
    * Update test1.js
    
    * Update Test1.yml
    
    * final versoin
    
    * Update connection.js
    
    * Delete package-lock.json
    
    * Delete package.json
    
    * Delete .DS_Store
    
    * add parker
    
    * successful version 1
    
    * update packer
    
    * finish packer 1
    
    * test first time
    
    * Update status-check.yml
    
    * Update status-check.yml
    
    * relocate the connection.js
    
    * update credential info
    
    * 03 (#36)
    
    * Update README.md
    
    * update new status check
    
    * Update connection-check.yml
    
    * Update connection-check.yml
    
    * update
    
    * update workflow
    
    * update
    
    * test1
    
    * update
    
    * update
    
    * Delete test-draft1.yml
    
    * Update connection-check.yml
    
    * Update connection-check.yml
    
    * Update connection-check.yml
    
    * Update connection-check.yml
    
    * Update connection-check.yml
    
    * Update connection-check.yml
    
    * Update connection-check.yml
    
    * Update connection-check.yml
    
    * update test1
    
    * update
    
    * Update test1.js
    
    * update test 1
    
    * Update test1.js
    
    * Update Test1.yml
    
    * final versoin
    
    * Delete package-lock.json
    
    * Delete package.json
    
    * Delete .DS_Store
    
    * Update connection.js
    
    ---------
    
    Co-authored-by: DanaKwoh <danag@myhost.local>
    
    ---------
    
    Co-authored-by: DanaKwoh <danag@myhost.local>

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.MYSQL_HOST,
   port: process.env.MYSQL_PORT,
   dialect: 'mysql'
