3d4c80f21d406392e6d37b513bca7792170a7391
DanaKwoh <guo0214@126.com>
2024-03-20 19:18:07 -0400

    add winston

--- app.js
+++ app.js
@@ -3,28 +3,39 @@ const bodyParser = require('body-parser');
 const bootstrap = require('./models/User');
 const healthRouter = require('./routes/healthRouters.js');
 const userRouter = require('./routes/userRouters.js');
-const checkHealthMiddleware = require('./middlewares/checkHealthMiddleware'); // Import checkHealthMiddleware
+const checkHealthMiddleware = require('./middlewares/checkHealthMiddleware'); 
 const authenticateUser = require('./middlewares/authMiddleware.js');
+const winston = require('winston'); 
+
 const app = express();
 
 const PORT = process.env.PORT;
 
 app.use(bodyParser.json());
 
+// Initialize winston logger
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(), // Format logs in JSON
+  transports: [
+    new winston.transports.Console(), // Log to console
+    new winston.transports.File({ filename: 'combined.log' }) // Log to a file
+  ]
+});
+
 // Initializing 
 const initializeApp = async () => {
   try {
     // Bootstrap the database
-    await bootstrap.sync({ alter: true }); 
-    console.log('Bootstrap the Database successfully!');
+    await bootstrap.sync({ alter: true });
+    logger.info('Bootstrap the Database successfully!'); // Log initialization success
   } catch (error) {
-    console.error('Error initializing app:', error.message);
+    logger.error(`Error initializing app: ${error.message}`); // Log initialization error
   }
 };
 
 // Attach health router with health check middleware
 app.use('/healthz', checkHealthMiddleware, healthRouter);
-
 // Attach user router
 app.use('/v1/user', userRouter);
 // Authentication middleware for GET and PUT request
@@ -38,18 +49,18 @@ app.use('/v1/user', (req, res, next) => {
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-  console.error('Error:', err);
+  logger.error(`Error: ${err.message}`); // Log error
   res.status(err.status).json({ error: err.message });
 });
 
 // Run the server
-const server = app.listen(PORT,'0.0.0.0', async () => {
+const server = app.listen(PORT, '0.0.0.0', async () => {
   try {
     await initializeApp();
     const address = server.address(); // Get the address info
-    console.log(`Running on the port:${PORT}`);
+    logger.info(`Running on the port: ${PORT}`); // Log server startup
   } catch (error) {
-    console.error("Error during server startup:", error);
+    logger.error(`Error during server startup: ${error.message}`); // Log server startup error
     process.exit(1);
   }
 });
